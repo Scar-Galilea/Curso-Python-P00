f7d2f211adaac72d45bf1e325db95ae61b7fffcd
Scar-Galilea <floresmariana475@gmail.com>
2025-03-05 15:54:22 -0600

    Agregar archivos readme

---
+++ Clases_y_objetos/Persona.py
@@ -0,0 +1,48 @@
+"""
+Nombre: Galilea Peralta Contreras.
+Fecha: 05 de marzo del 2025.
+
+Descripción:
+
+"""
+
+
+def caminar() -> None:
+    print("Estoy caminando.")
+
+
+class Persona:
+    # El self se refiere a uno mismo, en este caso al objeto que estamos costruyendo.
+    def __init__(self, nombre: str, edad: int, altura: float, peso: float):
+        # Asignar la variable como un objeto.
+        self.nombre = nombre
+        self.edad = edad
+        self.altura = altura
+        self.peso = nombre
+        # S e construyo a una persona
+
+    # Mis metodos:
+
+    def comer(self) -> None:
+        print("Estoy comiendo.")
+
+    def jugar(self) -> None:
+        print("Estoy jugando.")
+
+    def dormir(self) -> None:
+        print("Estoy durmiendo.")
+
+def main() -> None:
+    """
+    Función principal.
+    """
+    # Las clases siempre van en mayuscula.
+
+    galilea = Persona("Galilea",19,1.60,56)
+
+    print(galilea.nombre)
+    caminar()
+
+""" %%%%%%%     CÓDIGO A NIVEL DE MÓDULO    %%%%%%%%%%%%%%%%%%%%% """
+if __name__ == '__main__':
+    main()
