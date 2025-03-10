65c340410642f3744c60ba5c7b7c05522eb76062
Scar-Galilea <floresmariana475@gmail.com>
2025-03-10 14:05:13 -0600

    Archivos codewars

---
+++ Codedex/Hello World/Initials.py
@@ -0,0 +1,46 @@
+"""
+Nombre: Galilea Peralta Contreras.
+Fecha: 07 de marzo del 2025.
+
+Descripción:
+Crea un programa inicials.py que muestre tus iniciales en letras de bloque.
+
+Primero, comience el programa con un comentario que diga un hecho divertido sobre usted.
+
+Entonces crea tus letras de bloque con tus iniciales.
+
+Por ejemplo, si tu nombre es Dua Lipa, tus iniciales serían D y L, y tus letras de bloque se verían así:
+
+DDDD   L
+D   D  L
+D   D  L
+D   D  L
+D   D  L
+D   D  L
+DDDD   LLLLL
+"""
+
+def main() -> None:
+    """
+    Función principal.
+    """
+
+    # Este programa va a crear un bloque con mis iniciales.
+    # Mi nombre es Galilea y la inicial seria una G.
+
+    print('GGGGGGGGGGGGGG')
+    print('G')
+    print('G')
+    print('G')
+    print('G')
+    print('GGGGGGGGGGGGGG')
+    print('G            G')
+    print('G            G')
+    print('G            G')
+    print('GGGGGGGGGGGGGG')
+    print('             G')
+
+
+""" %%%%%%%     CÓDIGO A NIVEL DE MÓDULO    %%%%%%%%%%%%%%%%%%%%% """
+if __name__ == '__main__':
+    main()
