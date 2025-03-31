5d3e3e05b06b2d4e31f4e0a0084e500871beb0bf
Scar-Galilea <floresmariana475@gmail.com>
2025-03-31 16:01:58 -0600

    Archivos codewars

---
+++ Codewars/Draw_stairs_8_kyu.py
@@ -0,0 +1,71 @@
+"""
+Nombre: Galilea Peralta Contreras.
+Fecha: 31 de marzo del 2025.
+
+Descripción:
+Given a number n, draw stairs using the letter "I", n tall and n wide, with the tallest in the top left.
+
+For example n = 3 result in:
+
+"I\n I\n  I"
+
+or printed:
+
+I
+ I
+  I
+
+Another example, a 7-step stairs should be drawn like this:
+
+I
+ I
+  I
+   I
+    I
+     I
+      I
+
+"""
+
+def draw_stairs(n:int):
+    espacio = "/n"
+    caracter = 'I'
+    for i in range(n):
+        cadena = caracter + espacio
+        espacio = espacio + espacio
+        caracter = caracter + caracter
+
+    return cadena
+
+
+def cadena_a_entero(cadena: str) -> int | None:
+    """
+       Muestra el menu del programa
+       :param cadena: Lo que ingresa el usuario
+       :return: Un número entero o None
+       """
+    no_guiones = cadena.count("-")
+    revisar_cadena = cadena.lstrip("-")
+    if revisar_cadena.isnumeric() and no_guiones in(0,1) :
+        return  int(cadena)
+    else:
+        return None
+
+def main() -> None:
+    """
+    Función principal.
+    """
+
+    n = input("Ingrese el número de saltos de linea: ")
+    n = cadena_a_entero(n)
+    while  n  == None:
+        n = input(f"Vuelve intentarlo: ")
+        n = cadena_a_entero(n)
+
+    print(draw_stairs(n))
+
+
+
+""" %%%%%%%     CÓDIGO A NIVEL DE MÓDULO    %%%%%%%%%%%%%%%%%%%%% """
+if __name__ == '__main__':
+    main()
