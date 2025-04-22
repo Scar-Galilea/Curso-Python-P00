31f315b7d28584e88ecb2a00444b2486e8c005d9
Scar-Galilea <floresmariana475@gmail.com>
2025-04-22 15:04:00 -0600

    Archivo de codewars

---
+++ Codewars/Parcial_2/8_kyu/Limpieza.py
@@ -0,0 +1,33 @@
+"""
+Nombre: Galilea Peralta Contreras.
+Fecha: 22 de abril del 2025.
+
+Descripción:
+Su jefe decidió ahorrar dinero comprando algún software de reconocimiento óptico de frecuencia cortada para escanear en el texto de las novelas antiguas en su base de datos. Al principio parece capturar las palabras bien, pero rápidamente te das cuenta de que arroja muchos números a lugares al azar en el texto.
+Ejemplos (avance de entrada - salida)
+
+'! !'                 -> '! !'
+'123456789'           -> ''
+'This looks5 grea8t!' -> 'This looks great!'
+
+Sus compañeros de trabajo acosados están buscándole una solución para tomar este texto confuso y eliminar todos los números. Su programa tomará una cuerda y limpiará todos los personajes numéricos, y devolverá una cuerda con espaciamiento y caracteres especiales ~#$%^&!@*():;"'.,?todos intactos.
+
+"""
+
+def string_clean(s):
+    total = ""
+    for i in s:
+        if not i.isnumeric():
+            total = total + i
+    return total
+
+def main() -> None:
+    """
+    Función principal.
+    """
+    n = input("Ingrese: ") # Solicita la entrada del usuario.
+    print(string_clean(n))
+
+""" %%%%%%%     CÓDIGO A NIVEL DE MÓDULO    %%%%%%%%%%%%%%%%%%%%% """
+if __name__ == '__main__':
+    main()
